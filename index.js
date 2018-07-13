2e0ce6ac3617560a671f76c25ff8a4c60b3dfce9
Laura Kulas <laura.kulas@gmail.com>
2018-07-12 20:30:14 -0700

    added accessibility features

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ function displayData(data) {
   const results = data.items.map(function(item, index) {
     return generateResult(item);
   });
+  $('.js-search-number').html(`<h2>Results:</h2>
+    <p>Showing ${data.pageInfo.resultsPerPage} of ${data.pageInfo.totalResults} total</p>`);
+  $('.js-search-number').prop('hidden', false);
   $('.js-search-results').html(results);
+  $('.js-search-results').prop('hidden', false);
 }
 
 function generateResult(result) {
-  return `<div><a href="https://www.youtube.com/watch?v=${result.id.videoId}"><img src="${result.snippet.thumbnails.medium.url}"></a><p><a href="https://www.youtube.com/watch?v=${result.id.videoId}">${result.snippet.title}</a> by <a href="https://www.youtube.com/channel/${result.snippet.channelId}">${result.snippet.channelTitle}</a></p></div>`;
+  return `
+  <div>
+    <a href="https://www.youtube.com/watch?v=${result.id.videoId}">
+      <img src="${result.snippet.thumbnails.medium.url}" alt="Thumbnail image for video ${result.snippet.title}">
+    </a>
+    <p>
+      <a href="https://www.youtube.com/watch?v=${result.id.videoId}">${result.snippet.title}</a> 
+      by <a href="https://www.youtube.com/channel/${result.snippet.channelId}">${result.snippet.channelTitle}</a>
+    </p>
+  </div>`;
 }
 
 function watchSubmit() {
